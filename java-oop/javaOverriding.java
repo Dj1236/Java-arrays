1d8f5fa15404875e8ffdda08ec59a70d9d6c86fa
dhairya_joshi <joshidhairya79@gmail.com>
2024-03-11 10:32:46 +0530

    javaOverriding

---
+++ java-oop/javaOverriding.java
@@ -0,0 +1,19 @@
+public class javaOverriding {
+    /* If a class inherits a method from its superclass, then there is a chance to override the method provided that it is not marked final.
+
+Benefit of Overriding in Java
+The benefit of overriding is: ability to define a behavior that's specific to the subclass type, which means a subclass can implement a parent class method based on its requirement.
+
+In object-oriented terms, overriding means to override the functionality of an existing method.
+
+Java Method Overriding
+Method overriding allows us to achieve run-time polymorphism and is used for writing specific definitions of a subclass method that is already defined in the superclass.
+
+The method is superclass and overridden method in the subclass should have the same declaration signature such as parameters list, type, and return type.
+
+Usage of Java Method Overriding
+Following are the two important usages of method overriding in Java:
+
+Method overriding is used for achieving run-time polymorphism.
+Method overriding is used for writing specific definition of a subclass method (this method is known as the overridden method). */
+}
